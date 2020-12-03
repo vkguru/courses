5779cbf456b674ccee9b439f5d9f85e941caba8e
vkguru <vnwoguru@gmail.com>
2020-12-03 18:08:57 +0100

    responsiveness edit

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function notFound() {
   return (
-    <div style={{textAlign: 'center', paddingTop: '2rem'}}>
+    <div style={{textAlign: 'center', paddingTop: '2rem', width: '80%', margin: 'auto'}}>
       <h4 style={{fontSize: '2rem', fontFamily: 'Gilroy SemiBold'}}>The page you are looking for does not exist</h4>
       <Link to="/" className="courses-link">Home</Link>
     </div>
